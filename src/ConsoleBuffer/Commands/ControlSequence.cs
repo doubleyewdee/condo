fe6bb2b260135c23e7de0bf30d191dbdb5ab0240
Chip Locke <wd@teleri.net>
2018-12-19 19:06:42 -0800

    fix a couple small things with contributed CSI perf improvements

--- src/ConsoleBuffer/Commands/ControlSequence.cs
+++ src/ConsoleBuffer/Commands/ControlSequence.cs
@@ -1,8 +1,6 @@
 namespace ConsoleBuffer.Commands
 {
     using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
 
     public abstract class ControlSequence : Base
     {
@@ -11,22 +9,17 @@ namespace ConsoleBuffer.Commands
             // This 'should' be more than enough slots for parameters,
             // if we get more than this, it will be considered bad data
             // and throw an exception.
-            private static readonly int maxParameters = 32;
-            public static readonly int errorValue = -1; // eventually gets converted to unsigned, so this can be an error
+            private const int MaxParameters = 32;
+            public const int ErrorValue = -1; // eventually gets converted to unsigned, so this can be an error
 
             public int GetValue(int offset, int defaultValue = -1, int maxValue = int.MaxValue)
             {
-                if(offset < this.Count)
+                if (offset < this.Count)
                 {
                     var num = this.Parameters[offset];
 
                     // If we had a parse error, then return the default
-                    if (num == errorValue)
-                    {
-                        return defaultValue;
-                    }
-
-                    return Math.Max(defaultValue, Math.Min(num, maxValue));
+                    return num == ErrorValue ? defaultValue : Math.Max(defaultValue, Math.Min(num, maxValue));
                 }
 
                 return defaultValue;
@@ -35,43 +28,47 @@ namespace ConsoleBuffer.Commands
             public void Clear()
             {
                 this.Count = 0;
-                for (int idx = 0; idx < maxParameters; idx++)
+                for (var idx = 0; idx < MaxParameters; idx++)
                 {
                     this.Parameters[idx] = 0;
                 }
             }
 
             public int Count { get; set; }
-            public int[] Parameters { get; set; } = new int[maxParameters];
+            public int[] Parameters { get; set; } = new int[MaxParameters];
         }
 
         public static Base Create(char command, string bufferData)
         {
-            //Trace.WriteLine($"Parsing sequence ^[[{bufferData}{command}");
-            switch (command)
+            try
             {
-            case 'A':
-            case 'B':
-            case 'C':
-            case 'D':
-                return new CursorMove(bufferData, command);
-            case 'G':
-            case 'd':
-            case 'H':
-            case 'f':
-                return new SetCursorPosition(bufferData, command);
-            case 'h':
-            case 'l':
-                return new SetMode(bufferData, command == 'h');
-            case 'J':
-                return new EraseIn(bufferData, EraseIn.EraseType.Display);
-            case 'K':
-                return new EraseIn(bufferData, EraseIn.EraseType.Line);
-            case 'm':
-                return new SetGraphicsRendition(bufferData);
-            case 'X':
-                return new EraseCharacter(bufferData);
+                switch (command)
+                {
+                case 'A':
+                case 'B':
+                case 'C':
+                case 'D':
+                    return new CursorMove(bufferData, command);
+                case 'G':
+                case 'd':
+                case 'H':
+                case 'f':
+                    return new SetCursorPosition(bufferData, command);
+                case 'h':
+                case 'l':
+                    return new SetMode(bufferData, command == 'h');
+                case 'J':
+                    return new EraseIn(bufferData, EraseIn.EraseType.Display);
+                case 'K':
+                    return new EraseIn(bufferData, EraseIn.EraseType.Line);
+                case 'm':
+                    return new SetGraphicsRendition(bufferData);
+                case 'X':
+                    return new EraseCharacter(bufferData);
+                }
             }
+            catch (ArgumentException) { } // if parsing fails due to bad sequence data we return unsupported.
+
             return new Unsupported($"^[[{bufferData}{command}");
         }
 
@@ -102,14 +99,14 @@ namespace ConsoleBuffer.Commands
             var idx = startIndex;
             var len = bufferData.Length;
 
-            // Parses the string in bufferData to extract the int's represented by text,
-            // it will do it without an allocations.  bufferData takes the form of
+            // Parses the string in bufferData to extract the ints represented by text,
+            // it will do it without any allocations.  bufferData takes the form of
             // NUM;NUM;NUM... for an unknown number of of NUM's
             while (idx < len)
             {
                 // Parse the number
                 var val = 0;
-                bool neg = false;
+                var neg = false;
                 while (idx < len && bufferData[idx] != ';')
                 {
                     if (bufferData[idx] == '-')
@@ -118,7 +115,7 @@ namespace ConsoleBuffer.Commands
                         idx++;
                     }
 
-                    char c = bufferData[idx];
+                    var c = bufferData[idx];
 
                     // Check for bogus characters
                     if ((c < '0' || c > '9') && c != ';')
@@ -129,16 +126,15 @@ namespace ConsoleBuffer.Commands
                     // Take the previous character value (or accumulated value), multiply by 10
                     // add in the current characteres numerical value.
                     val = val * 10 + (c - '0');
-                    idx++;  // next char
-
+                    ++idx;  // next char
                 }
 
-                idx++; // get past the ';'
+                ++idx; // get past the ';'
 
                 // Check for ushort overflow
                 if (val > ushort.MaxValue || neg == true)
                 {
-                    val = ParsedParameters.errorValue;
+                    val = ParsedParameters.ErrorValue;
                 }
 
                 this.Parameters.Parameters[this.Parameters.Count++] = val;
